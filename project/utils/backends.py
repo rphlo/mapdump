e06d893e3e52539f2a8ad7dfc59a7b4b1d04f0f3
Raphaël Stefanini <rphl@rphl.net>
2020-01-26 19:49:05 +0200

    Add auth endpoints

---
+++ project/utils/backends.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
+
+
+class CaseInsensitiveModelBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        user_model = get_user_model()
+        if username is None:
+            username = kwargs.get(user_model.USERNAME_FIELD)
+        try:
+            case_insensitive_username_field = '{}__iexact'.format(
+                user_model.USERNAME_FIELD
+            )
+            user = user_model._default_manager.get(
+                **{case_insensitive_username_field: username}
+            )
+        except user_model.DoesNotExist:
+            # Run the default password hasher once to reduce the timing
+            # difference between an existing and a non-existing user (#20760).
+            user_model().set_password(password)
+        else:
+            if user.check_password(password) \
+                    and self.user_can_authenticate(user):
+                return user
