bbeeaa5aef73e55437466d92f52f3722c53b88fa
Raphaël Stefanini <raphael.stefanini@ecom.fi>
2020-02-24 16:01:29 +0200

    Prepare for cypress end to end testing tests

--- docker/react.dockerfile
+++ docker/react.dockerfile
@@ -8,7 +8,7 @@ COPY ./package.json ./yarn.lock ./
 ## install only the packages defined in the package-lock.json (faster than the normal npm install)
 RUN yarn install
 # Copy the contents of the project to the image
-COPY . .
+
 # Run 'npm start' when the container starts.
 ENV REACT_APP_API_URL='http://localhost'
 CMD ["yarn", "run", "start"]
--- frontend/src/components/LatestRoutes.js
+++ frontend/src/components/LatestRoutes.js
@@ -13,24 +13,30 @@ const LatestRoute = () => {
     }, [])
     
     return (
-        <>
+      <>
         <h3>Latest Routes</h3>
         <div className="container" style={{textAlign: 'left'}}>
-            <div className="row">
-                {routes.map(r=>(
-                    <div key={r.id} className="col-12 col-md-4"><div className="card">
+            {!routes.length ? 
+               (
+                  <div style={{textAlign: 'center'}}><span>No routes have been yet uploaded...</span></div>
+                ) : (
+                  <div className="row">
+                    {routes.map(r=>(
+                    <div key={r.id} className="col-12 col-md-4">
+                      <div className="card">
                         <Link to={'/routes/'+r.id}><img className="card-img" src={r.map_thumbnail_url} alt="map thumbnail"></img></Link>
                         <div className="card-body">
-                        <h5 className="card-title"><span className={("flag-icon flag-icon-"+r.country.toLowerCase())}></span> {r.name}</h5>
-                        <p className="card-text">By <Link to={'/athletes/'+r.athlete.username}>{r.athlete.first_name} {r.athlete.last_name}</Link></p>
-                        <p className="card-text">{moment(r.start_time).utcOffset(r.tz).format('dddd, MMMM Do YYYY, HH:mm')}</p>
+                          <h5 className="card-title"><span className={("flag-icon flag-icon-"+r.country.toLowerCase())}></span> {r.name}</h5>
+                          <p className="card-text">By <Link to={'/athletes/'+r.athlete.username}>{r.athlete.first_name} {r.athlete.last_name}</Link></p>
+                          <p className="card-text">{moment(r.start_time).utcOffset(r.tz).format('dddd, MMMM Do YYYY, HH:mm')}</p>
                         </div>
-                    </div>
-                </div>))}    
-            </div>
-            {!routes.length && <div style={{textAlign: 'center'}}><span>No routes have been yet uploaded</span></div>}
+                      </div>
+                    </div>))}
+                  </div>
+                )   
+            }
         </div>
-        </>
+      </>
     ) 
 }
 
