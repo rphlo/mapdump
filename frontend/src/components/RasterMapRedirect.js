668e682e9ddf51e365e09bbb1b508d75f5303c44
Raphaël Stefanini <rphl@rphl.net>
2020-09-05 10:57:51 +0300

    Cleaner viewer

---
+++ frontend/src/components/RasterMapRedirect.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const RasterMapRedirect = ({match, history}) => {
+    history.push('/routes/' + match.params.uid);
+    return (<></>)
+}
+
+export default RasterMapRedirect
