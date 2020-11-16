6e4016dddf360f92d84d10936799ad5e28540a78
Raphaël Stefanini <raphael.stefanini@ecom.fi>
2020-11-16 17:54:01 +0200

    Add command

---
+++ project/routedb/management/commands/remove_cached_maps.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from routedb.models import Route
+
+
+class Command(BaseCommand):
+    help = 'Remove image cache'
+
+    def handle(self, *args, **options):
+        qs = Route.objects.all().update(
+            has_image_w_header=False,
+            has_image_w_route=False,
+            has_image_w_header_route=False,
+            has_image_thumbnail=False,
+            has_image_blank=False,
+        )
+        self.stdout.write(self.style.SUCCESS('Done'))
