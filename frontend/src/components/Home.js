4bb7fb8af5b5699fff49eb548628a3e39143e1ad
Raphaël Stefanini <raphael.stefanini@ecom.fi>
2021-01-24 12:08:24 +0200

    Remove sponsor banner

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -8,12 +8,14 @@ const Home = () => {
     const { username } = globalState.user
     return (
     <div style={{textAlign:'center'}} >
+      {/*
       <div className="alert alert-warning">
         <p><i className="fa fa-warning"></i> Karttamuovi is a open source project, available to all for free, and hosted by vonlunteer.</p>
         <p>You can help contribute to the payment of the cost of hosting this solution through monthly donations.</p>
         <p>Follow this link if you want to contribute <a href="https://github.com/sponsors/rphlo"><i className="fa fa-heart"></i> Github Sponsor</a></p>
       </div>
       <hr/>
+      */}
       <Link to="/new"><button className="btn btn-primary"><i className="fas fa-plus"></i> Create New Route</button></Link><> </>
       <Link to="/map"><button className="btn btn-info"><i className="fas fa-globe"></i> Browse Maps</button></Link>
       {username && <><hr/>
