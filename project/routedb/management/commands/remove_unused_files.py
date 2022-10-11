00f0766213b4dbfe3d406b02a0a34a7eeecc80dd
Raphael Stefanini <raphael@rphlo.com>
2022-10-11 10:12:00 +0300

    Remove all routes images as they arent used

--- project/routedb/management/commands/remove_unused_files.py
+++ project/routedb/management/commands/remove_unused_files.py
@@ -1,31 +1,29 @@
 from django.conf import settings
 from django.core.management.base import BaseCommand
-from routedb.models import Route
-from utils.s3 import s3_delete_key, s3_key_exists
+from utils.s3 import s3_delete_key
 
 
 class Command(BaseCommand):
     help = "Remove image file from prev version"
 
+    def scan_map_directory(self):
+        # Should use v2 but wasabi fails to list all files with it
+        # paginator = s3.get_paginator('list_objects_v2')
+        paginator = self.s3.get_paginator("list_objects")
+        kwargs = {
+            "Bucket": settings.AWS_S3_BUCKET,
+            "Prefix": "routes/",
+        }
+        for page in paginator.paginate(**kwargs):
+            try:
+                contents = page["Contents"]
+            except KeyError:
+                break
+            for obj in contents:
+                key = obj["Key"]
+                yield key
+
     def handle(self, *args, **options):
-        qs = Route.objects.all()
-        for route in qs:
-            file_path = route.raster_map.path + "_thumb"
-            if s3_key_exists(file_path, settings.AWS_S3_BUCKET):
-                s3_delete_key(file_path, settings.AWS_S3_BUCKET)
-            file_path = route.images_path + "_header"
-            if s3_key_exists(file_path, settings.AWS_S3_BUCKET):
-                s3_delete_key(file_path, settings.AWS_S3_BUCKET)
-            file_path = route.images_path + "_route"
-            if s3_key_exists(file_path, settings.AWS_S3_BUCKET):
-                s3_delete_key(file_path, settings.AWS_S3_BUCKET)
-            file_path = route.images_path + "_header_route"
-            if s3_key_exists(file_path, settings.AWS_S3_BUCKET):
-                s3_delete_key(file_path, settings.AWS_S3_BUCKET)
-            file_path = route.images_path
-            if s3_key_exists(file_path, settings.AWS_S3_BUCKET):
-                s3_delete_key(file_path, settings.AWS_S3_BUCKET)
-            file_path = route.images_path + "_blank"
-            if s3_key_exists(file_path, settings.AWS_S3_BUCKET):
-                s3_delete_key(file_path, settings.AWS_S3_BUCKET)
+        for filename in self.scan_map_directory():
+            s3_delete_key(filename, settings.AWS_S3_BUCKET)
         self.stdout.write(self.style.SUCCESS("Done"))
