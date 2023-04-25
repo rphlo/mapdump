a70255f2cd83659ddd2a2949f46a5b4eb9ddfa0a
Raphaël Stefanini <rphlo@users.noreply.github.com>
2023-04-25 09:07:40 +0300

    Update CalibrationPreview.js

--- project/frontend/src/components/CalibrationPreview.js
+++ project/frontend/src/components/CalibrationPreview.js
@@ -2,57 +2,209 @@ import React, { useEffect, useState } from "react";
 import * as L from "leaflet";
 import "../utils/Leaflet.ImageTransform";
 
-L.TileLayer.Common = L.TileLayer.extend({
-  initialize: function (options) {
-    L.TileLayer.prototype.initialize.call(this, this.url, options);
-  },
-});
-L.TileLayer["osm"] = L.TileLayer.Common.extend({
-  url: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
-  options: {
+var backdropMaps = {
+  osm: L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
-      "\u0026copy\u003B \u003Ca href\u003D\u0022http://openstreetmap.org\u0022\u003EOpenStreetMap\u003C/a\u003E contributors, \u003Ca href\u003D\u0022http://creativecommons.org/licenses/by\u002Dsa/2.0/\u0022\u003ECC\u002DBY\u002DSA\u003C/a\u003E",
-  },
-});
-L.TileLayer["gmap-street"] = L.TileLayer.Common.extend({
-  url: "https://mt0.google.com/vt/x\u003D{x}\u0026y\u003D{y}\u0026z\u003D{z}",
-  options: { attribution: "\u0026copy\u003B Google" },
-});
-L.TileLayer["gmap-hybrid"] = L.TileLayer.Common.extend({
-  url: "https://mt0.google.com/vt/lyrs\u003Dy\u0026hl\u003Den\u0026x\u003D{x}\u0026y\u003D{y}\u0026z\u003D{z}",
-  options: { attribution: "\u0026copy\u003B Google" },
-});
-L.TileLayer["finland-topo"] = L.TileLayer.Common.extend({
-  url: "https://tiles.kartat.kapsi.fi/peruskartta/{z}/{x}/{y}.jpg",
-  options: { attribution: "\u0026copy\u003B National Land Survey of Finland" },
-});
-L.TileLayer["mapant-fi"] = L.TileLayer.Common.extend({
-  url: "https://wmts.mapant.fi/wmts_EPSG3857.php?z\u003D{z}\u0026x\u003D{x}\u0026y\u003D{y}",
-  options: { attribution: "MapAnt and National Land Survey of Finland" },
-});
-L.TileLayer["norway-topo"] = L.TileLayer.Common.extend({
-  url: "https://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers\u003Dtopo4\u0026zoom\u003D{z}\u0026x\u003D{x}\u0026y\u003D{y}",
-  options: { attribution: "" },
-});
-L.TileLayer["mapant-no"] = L.TileLayer.Common.extend({
-  url: "https://mapant.no/osm-tiles/{z}/{x}/{y}.png",
-  options: { attribution: "MapAnt Norway" },
-});
-L.TileLayer["mapant-es"] = L.tileLayer.wms("https://mapant.es/wms", {
-  layers: "mapant.es",
-  format: "image/png",
-  version: "1.3.0",
-  transparent: true,
-  attribution: "&copy; MapAnt.es",
-});
-L.TileLayer["world-topo"] = L.TileLayer.Common.extend({
-  url: "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
-  options: { attribution: "&copy; OpenTopoMap (CC-BY-SA)" },
-});
-L.TileLayer["world-topo-alt"] = L.TileLayer.Common.extend({
-  url: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}",
-  options: { attribution: "&copy; ArcGIS Online" },
-});
+      'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    className: "wms256",
+  }),
+  "gmap-street": L.tileLayer("https://mt0.google.com/vt/x={x}&y={y}&z={z}", {
+    attribution: "&copy; Google",
+    className: "wms256",
+  }),
+  "gmap-hybrid": L.tileLayer(
+    "https://mt0.google.com/vt/lyrs=y&hl=en&x={x}&y={y}&z={z}",
+    {
+      attribution: "&copy; Google",
+      className: "wms256",
+    }
+  ),
+  "gmap-terrain": L.tileLayer(
+    "https://mt0.google.com/vt/lyrs=p&hl=en&x={x}&y={y}&z={z}",
+    {
+      attribution: "&copy; Google",
+      className: "wms256",
+    }
+  ),
+  "topo-fi": L.tileLayer(
+    "https://tiles.kartat.kapsi.fi/peruskartta/{z}/{x}/{y}.jpg",
+    {
+      attribution: "&copy; National Land Survey of Finland",
+      className: "wms256",
+    }
+  ),
+  "mapant-fi": L.tileLayer(
+    "https://wmts.mapant.fi/wmts_EPSG3857.php?z={z}&x={x}&y={y}",
+    {
+      attribution: "&copy; MapAnt.fi and National Land Survey of Finland",
+      className: "wms256",
+    }
+  ),
+  "topo-no": L.tileLayer(
+    "https://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=topo4&zoom={z}&x={x}&y={y}",
+    {
+      attribution: "",
+      className: "wms256",
+    }
+  ),
+  "topo-uk": L.tileLayer(
+    "https://api.os.uk/maps/raster/v1/zxy/Outdoor_3857/{z}/{x}/{y}.png?key=5T04qXvNDxLX1gCEAXS0INCgLvczGRYw",
+    {
+      attribution: "&copy; Ordnance Survey",
+      className: "wms256",
+    }
+  ),
+  "mapant-no": L.tileLayer("https://mapant.no/osm-tiles/{z}/{x}/{y}.png", {
+    attribution: "&copy; MapAnt.no",
+    className: "wms256",
+  }),
+  "mapant-ch": L.tileLayer(
+    "https://tile-proxy.routechoices.com/ch/{z}/{x}/{y}.jpg",
+    {
+      attribution: "&copy; MapAnt.ch",
+      className: "wms256",
+    }
+  ),
+  "mapant-se": L.tileLayer(
+    "/https://tile-proxy.routechoices.com/se/{z}/{x}/{y}.jpg",
+    {
+      attribution: "&copy; gokartor.se",
+      className: "wms256",
+    }
+  ),
+  "topo-fr": L.tileLayer(
+    "https://wxs.ign.fr/choisirgeoportail/geoportail/wmts?layer=GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2&style=normal&tilematrixset=PM&Service=WMTS&Request=GetTile&Version=1.0.0&Format=image%2Fpng&tilematrix={z}&tilecol={x}&tilerow={y}",
+    {
+      attribution:
+        '<a href="https://www.ign.fr/" target="_blank">&copy; IGN France</a>',
+      className: "wms256",
+    }
+  ),
+  "mapant-es": L.tileLayer.wms("https://mapant.es/wms", {
+    layers: "mapant.es",
+    format: "image/png",
+    version: "1.3.0",
+    transparent: true,
+    attribution: "&copy; MapAnt.es",
+    className: "wms256",
+  }),
+  "topo-world": L.tileLayer("https://tile.opentopomap.org/{z}/{x}/{y}.png", {
+    attribution: "&copy; OpenTopoMap (CC-BY-SA)",
+    className: "wms256",
+  }),
+  "topo-world-alt": L.tileLayer(
+    "https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}",
+    {
+      attribution: "&copy; ArcGIS Online",
+      className: "wms256",
+    }
+  ),
+};
+
+function cloneLayer(layer) {
+  function cloneOptions(options) {
+    var ret = {};
+    for (var i in options) {
+      var item = options[i];
+      if (item && item.clone) {
+        ret[i] = item.clone();
+      } else if (item instanceof L.Layer) {
+        ret[i] = cloneLayer(item);
+      } else {
+        ret[i] = item;
+      }
+    }
+    return ret;
+  }
+
+  function cloneInnerLayers(layer) {
+    var layers = [];
+    layer.eachLayer(function (inner) {
+      layers.push(cloneLayer(inner));
+    });
+    return layers;
+  }
+  var options = cloneOptions(layer.options);
+  // Renderers
+  if (layer instanceof L.SVG) {
+    return L.svg(options);
+  }
+  if (layer instanceof L.Canvas) {
+    return L.canvas(options);
+  }
+  // GoogleMutant GridLayer
+  if (L.GridLayer.GoogleMutant && layer instanceof L.GridLayer.GoogleMutant) {
+    var googleLayer = L.gridLayer.googleMutant(options);
+    layer._GAPIPromise.then(function () {
+      var subLayers = Object.keys(layer._subLayers);
+
+      for (var i in subLayers) {
+        googleLayer.addGoogleLayer(subLayers[i]);
+      }
+    });
+    return googleLayer;
+  }
+  // Tile layers
+  if (layer instanceof L.TileLayer.WMS) {
+    return L.tileLayer.wms(layer._url, options);
+  }
+  if (layer instanceof L.TileLayer) {
+    return L.tileLayer(layer._url, options);
+  }
+  if (layer instanceof L.ImageOverlay) {
+    return L.imageOverlay(layer._url, layer._bounds, options);
+  }
+  // Marker layers
+  if (layer instanceof L.Marker) {
+    return L.marker(layer.getLatLng(), options);
+  }
+  if (layer instanceof L.Circle) {
+    return L.circle(layer.getLatLng(), layer.getRadius(), options);
+  }
+  if (layer instanceof L.CircleMarker) {
+    return L.circleMarker(layer.getLatLng(), options);
+  }
+  if (layer instanceof L.Rectangle) {
+    return L.rectangle(layer.getBounds(), options);
+  }
+  if (layer instanceof L.Polygon) {
+    return L.polygon(layer.getLatLngs(), options);
+  }
+  if (layer instanceof L.Polyline) {
+    return L.polyline(layer.getLatLngs(), options);
+  }
+  if (layer instanceof L.GeoJSON) {
+    return L.geoJson(layer.toGeoJSON(), options);
+  }
+  if (layer instanceof L.FeatureGroup) {
+    return L.featureGroup(cloneInnerLayers(layer));
+  }
+  if (layer instanceof L.LayerGroup) {
+    return L.layerGroup(cloneInnerLayers(layer));
+  }
+  throw "Unknown layer, cannot clone this layer. Leaflet-version: " + L.version;
+}
+
+function getBaseLayers() {
+  return {
+    "Open Street Map": cloneLayer(backdropMaps["osm"]),
+    "Google Map Street": cloneLayer(backdropMaps["gmap-street"]),
+    "Google Map Satellite": cloneLayer(backdropMaps["gmap-hybrid"]),
+    "Google Map Terrain": cloneLayer(backdropMaps["gmap-terrain"]),
+    "Mapant Finland": cloneLayer(backdropMaps["mapant-fi"]),
+    "Mapant Norway": cloneLayer(backdropMaps["mapant-no"]),
+    "Mapant Spain": cloneLayer(backdropMaps["mapant-es"]),
+    "Mapant Sweden": cloneLayer(backdropMaps["mapant-se"]),
+    "Mapant Switzerland": cloneLayer(backdropMaps["mapant-ch"]),
+    "Topo Finland": cloneLayer(backdropMaps["topo-fi"]),
+    "Topo France": cloneLayer(backdropMaps["topo-fr"]),
+    "Topo Norway": cloneLayer(backdropMaps["topo-no"]),
+    "Topo UK": cloneLayer(backdropMaps["topo-uk"]),
+    "Topo World (OpenTopo)": cloneLayer(backdropMaps["topo-world"]),
+    "Topo World (ArcGIS)": cloneLayer(backdropMaps["topo-world-alt"]),
+  };
+}
+
 
 function round5(x) {
   return Math.round(x * 1e5) / 1e5;
@@ -89,18 +241,8 @@ const CalibrationPreview = (props) => {
     const latlngs = routeData.map((pt) => pt.latlng.slice(0, 2));
     L.polyline(latlngs, { color: "red" }).addTo(tmpMapPreview);
 
-    const baseLayers = {};
-    const defaultLayer = new L.TileLayer["osm"]();
-    baseLayers["Open Street Map"] = defaultLayer;
-    baseLayers["Google Map Street"] = new L.TileLayer["gmap-street"]();
-    baseLayers["Google Map Satellite"] = new L.TileLayer["gmap-hybrid"]();
-    baseLayers["Mapant Finland"] = new L.TileLayer["mapant-fi"]();
-    baseLayers["Mapant Norway"] = new L.TileLayer["mapant-no"]();
-    baseLayers["Mapant Spain"] = L.TileLayer["mapant-es"];
-    baseLayers["Topo Finland"] = new L.TileLayer["finland-topo"]();
-    baseLayers["Topo Norway"] = new L.TileLayer["norway-topo"]();
-    baseLayers["Topo World (OpenTopo)"] = new L.TileLayer["world-topo"]();
-    baseLayers["Topo World (ArcGIS)"] = new L.TileLayer["world-topo-alt"]();
+    const baseLayers = getBaseLayers();
+    const defaultLayer = baseLayers["Open Street Map"];
 
     tmpMapPreview.addLayer(defaultLayer);
 
