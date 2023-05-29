43f1b6d938668acd79c5d6a6ba9398a5428e1243
Raphael Stefanini <raphael@rphlo.com>
2023-05-29 13:51:08 +0300

    Routes for tag list view

---
+++ project/frontend/src/components/RoutesForTag.js
@@ -0,0 +1,18 @@
+import React from "react";
+import LatestRoutes from "./LatestRoutes";
+import { Helmet } from "react-helmet";
+
+const RoutesForTags = ({ match }) => {
+  return (
+    <>
+      <Helmet>
+        <title>Mapdump.com</title>
+      </Helmet>
+      <div className="container main-container">
+        <LatestRoutes tag={match.params.uid} />
+      </div>
+    </>
+  );
+};
+
+export default RoutesForTags;
