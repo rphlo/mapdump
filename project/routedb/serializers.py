d1a57f1844fb8cdf5f88b02d484d6f80f84ab4b1
Raphael Stefanini <raphael@rphlo.com>
2023-09-14 19:28:52 +0300

    Fix bug ..

--- project/routedb/serializers.py
+++ project/routedb/serializers.py
@@ -239,8 +239,8 @@ class RouteSerializer(serializers.ModelSerializer):
     def save(self):
         super().save()
         instance = self.instance
-        self.instance.prefetch_route_extras()
-        super().save()
+        instance.prefetch_route_extras()
+        instance.save()
         comment = instance.comment
         instance.tags = ", ".join(
             hashtag_match.group(2).lower()
