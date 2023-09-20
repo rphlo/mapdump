9f8dca82df369fdadd9a134e06a57feb8f0a3f6a
Raphael Stefanini <raphael@rphlo.com>
2023-09-20 10:51:35 +0300

    precommit

--- project/routedb/apps.py
+++ project/routedb/apps.py
@@ -1,8 +1,9 @@
 from django.apps import AppConfig
 from routedb import patch
 
+
 class RouteDBConfig(AppConfig):
     name = "routedb"
-    
+
     def ready(self):
         patch.patch_requests_default_timeout()
